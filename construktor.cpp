0aa92fdb6ea6e3220c4ca5233a503d2839c4a64c
nopriterbang <novalpriya@gmail.com>
2024-05-13 19:31:37 +0700

    menambahkan class

--- construktor.cpp
+++ construktor.cpp
@@ -17,5 +17,45 @@ private:
     {
         nim = iNim;
     }        
+class Mahasiswa
+();
+Mahasiswa(string);
+Mahasiswa(int iNim, string iNama);
+void cetak();
+
 };
+
+Mahasiswa::Mahasiswa(string iNama){
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim,string iNama)
+{
+    nim = iNim;
+    nama = iNama;
+};
+
+void Mahasiswa::cetak(){
+ cout << "NIM = " << nim << endl;
+ cout << "Nama = " << nama << endl;
+ cout << endl;
+}
+
+int main()
+{
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Ronaldo");
+    Mahasiswa mhs4(30, "Messi");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+    
+    return 0;
+}
+
+
+
 
