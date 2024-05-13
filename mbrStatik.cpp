3b8acbec919002ce93ce72227c2bd9295da62201
nopriterbang <novalpriya@gmail.com>
2024-05-13 14:51:26 +0700

    menyelesaikan member statik

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -1,2 +1,47 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa
+{
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+
+    void printAll();
+    mahasiswa(string pnama):nama(pnama)
+    {
+        setID();
+    }
+};
+
+int mahasiswa::nim = 20;
+
+void mahasiswa::setID()
+{
+    id = nim++;
+}
+
+void mahasiswa::printAll()
+{
+    cout <<"ID = " << id << endl;
+    cout <<"Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi kurniawan");
+    mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+    return 0;
+}
