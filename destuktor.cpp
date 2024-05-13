4da5758e9f882d0800bdf356f2341cca47515463
nopriterbang <novalpriya@gmail.com>
2024-05-13 19:57:24 +0700

    menyelesaikan destructor

--- destuktor.cpp
+++ destuktor.cpp
@@ -14,3 +14,44 @@ public:
     void cetakData();
     void isiData();
 };
+
+// Definisi member Function
+Angka::Angka(int i) // Constructor
+{
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+Angka::~Angka() //Destructor
+{
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat array sudah dilepaskan." << endl;
+}
+
+void Angka::cetakData()
+{
+    for(int i = 1; i < panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void Angka::isiData()
+{
+    for(int i = 1; i < panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+int main()
+{
+    Angka belajarcpp(3); // Constructor dipanggil
+    Angka *ptrBelajarcpp = new Angka(5); // Constructor dipanggil
+    delete ptrBelajarcpp; // Destructor dipanggil
+
+    return 0;
+}
